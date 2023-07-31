e8f217f1dc5789b857effa93aa531aabee74a00a
Jash Tailor <59695729+jashtailor@users.noreply.github.com>
2023-07-31 22:05:56 +0530

    Update your_script.py

--- your_script.py
+++ your_script.py
@@ -1,15 +1,29 @@
 import random
+import os
 
 def generate_random_list():
-    random_list = random.sample(range(1, 101), 10)
-    return random_list
+    return random.sample(range(1, 101), 10)
 
-def run_at_midnight():
+def save_list_to_txt_file(version, random_list):
+    filename = f"v{version}.txt"
+    with open(filename, "w") as file:
+        for item in random_list:
+            file.write(str(item) + "\n")
+
+def run_at_midnight(version):
     # Generate a random list of 10 integers
     random_list = generate_random_list()
 
-    # Print the list
-    print("Random List at Midnight:", random_list)
+    # Save the list to a TXT file
+    save_list_to_txt_file(version, random_list)
+
+    # Print the list (optional)
+    print(f"Random List for v{version}:", random_list)
 
 if __name__ == "__main__":
-    run_at_midnight()
+    # Get the current version by counting the existing TXT files
+    version = 1
+    while os.path.exists(f"v{version}.txt"):
+        version += 1
+    
+    run_at_midnight(version)
